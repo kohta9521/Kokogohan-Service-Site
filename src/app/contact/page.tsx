25e397dd682ca5bc6b37f7dddc21195a9263431b
k_95.21 <kohta0421kochi@gmail.com>
2023-11-19 17:23:44 +0900

    add contact form company or personal change logic

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
 
 // scss import
 import styles from './Contact.module.scss'
-import SubPageHero from '@/components/templates/SubPageHero'
-import MainLayout from '@/components/templates/MainLayout'
-import SubPagePureLayout from '@/components/templates/SubPagePureLayout'
-
 
 // components import
-
+import SubPageHero from '@/components/templates/SubPageHero'
+import SubPagePureLayout from '@/components/templates/SubPagePureLayout'
+import SubPageContent from '@/components/templates/SubPageContent'
+import SubPageSecTitle from '@/components/atoms/SubPageSecTitle'
+import SubPageTitleBox from '@/components/molecules/SubPageTitleBox'
+import SubPageMainText from '@/components/atoms/SubPageMainText'
 
 
 const Contact = () => {
+
+    // formの切り替え
+    const [form, setForm] = useState<'personal' | 'company'>('personal');
+
+
     return(
         <div className={styles.about}>
             {/* TODO: pureLayoutとかのコンポーネントを作る　contactの中にcontactは変 */}
@@ -25,9 +31,48 @@ const Contact = () => {
                     title='お問い合わせ'
                     subTitle='このサービスについてお問い合わせいただけます。'
                     pageName='CONTACT'
-                >
-                    <h1>this space is contact children space</h1>
-                </SubPageHero>
+                />
+                <SubPageContent id={5}>
+                    <SubPageSecTitle id={1} text='Contact' color='red' />
+                    <SubPageTitleBox id={1} enTitle='FORM' jaTitle='お問い合わせフォーム' />
+                    <SubPageMainText
+                        id={1}
+                        text='
+                            ここではサービスについて、また、サービスを使用する上での不具合や要望などをお問い合わせいただけます。
+                            下記のフォームは法人のお客様と個人のお客様で内容が分かれていますので、お問い合わせの内容によって選択してください。
+                            また、お問い合わせの内容によっては、お返事にお時間をいただく場合がございます。あらかじめご了承ください。
+                        '
+                    />
+                    <div className={styles.formBox}>
+                        <div className={styles.flexButton}>
+                        <button
+                            className={`${styles.personalButton} ${form === 'personal' ? styles.active : ''}`}
+                            onClick={() => setForm('personal')}
+                        >
+                            個人の方
+                        </button>
+                        <button
+                            className={`${styles.companyButton} ${form === 'company' ? styles.active : ''}`}
+                            onClick={() => setForm('company')}
+                        >
+                            法人の方
+                        </button>
+                        </div>
+                        <div className={styles.formBox}>
+                        {form === 'personal' ? (
+                            <div>
+                            <h1>Person Form</h1>
+                            {/* ここに実際の個人の方向けのフォームを配置 */}
+                            </div>
+                        ) : (
+                            <div>
+                            <h1>Company Form</h1>
+                            {/* ここに実際の法人の方向けのフォームを配置 */}
+                            </div>
+                        )}
+                        </div>
+                    </div>
+                </SubPageContent>
             </SubPagePureLayout>
         </div>
     )
